413fb504e7a493e73cbdcf7d1a8b0a0736a69108
toki-oy <Leo@otsukayumiko-no-MacBook-Pro.local>
2013-07-03 19:29:41 +0900

    add file

---
+++ mygame/test.js
@@ -0,0 +1,10 @@
+function test(){
+    
+    var devices = ["iPhone","Android","パソコン"];
+    
+    for(var i=0; i<devices.length; i++){
+    console.log(i + "回目　:　"+devices[i]);
+    }
+}
+
+test();
