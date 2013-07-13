af15b108684263cf0770d9b83c1bbd6496077ffe
toki-oy <Leo@otsukayumiko-no-MacBook-Pro.local>
2013-07-13 12:04:45 +0900

    ball appear.
    I let a ball appear.

--- mygame/script.js
+++ mygame/script.js
@@ -1,42 +1,24 @@
 enchant();
+var ball = enchant.Class.create(enchant.Sprite,{
+    initialize: function(x,y){
+        enchant.Sprite.call(this,16,16);
+        this.scaleX = 2.5;
+        this.scaleY = 2.5;
+        this.image = game.assets['images/icon1.png'];
+        this.x = x; this.y = y; this.frame = 0;
+        game.rootScene.addChild(this);
+    }
+});
 window.onload = function() {
-    //ゲームプロジェクトの生成
-    var game = new Game(320, 320);
+    game = new Game(320, 320);
     game.fps = 16;
+    game.preload('images/icon1.png');
+    game.tick = 0;
     
-    //画像の読み込み
-    game.preload('http://enchantjs.com/assets/images/chara1.gif');
-    
-    //ロード完了時に呼ばれる
     game.onload = function() {
-        //スプライトの生成
-        var bear   = new Sprite(32, 32);
-        bear.image = game.assets['http://enchantjs.com/assets/images/chara1.gif'];
-        bear.tick  = 0;
-        bear.anim  = [10, 11, 10, 12];
-        game.rootScene.addChild(bear);
-        
-        //スプライトの定期処理
-        bear.addEventListener(Event.ENTER_FRAME, function() {
-            //スプライトのフレームの指定
-            bear.tick++;
-            bear.frame = bear.anim[bear.tick % 4];
-            
-            //右向き
-            if (bear.scaleX == 1) {
-                bear.x += 3;
-                //向き変更
-                if (bear.x > 320 - 32) bear.scaleX = -1;
-            }
-            //左向き
-            else {
-                bear.x -= 3;
-                //向き変更
-                if (bear.x < 0) bear.scaleX = 1;
-            }
-        });
-    };
-    
-    //ゲームの開始
+     ball = new ball(152, 240); //ボールを出現させる
+        game.rootScene.backgroundColor = 'black';
+    }
     game.start();
 };
+        
